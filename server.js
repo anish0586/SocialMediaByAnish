3946f8aa5c3f5f670819ff72f153cdefa025a65b
Anish Akhauri <anish.akhauri@gmail.com>
2020-05-21 00:41:54 -0700

    MiddleWare update

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+//app.use(express.json({ extended: false }));
+app.use(express.json());
 
 //app.get('/', (req, res) => res.send('API Running'));
 
